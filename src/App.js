933688b2774189ad42f101ee0aaf7cb2f07e5573
Ronit Pradhan <iamronit94@gmail.com>
2023-04-02 01:33:16 +0530

    Todo 1.0

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { Heading } from '@chakra-ui/react';
+import TodoList from './components/TodoList';
+import AddTodo from './components/AddTodo';
+import { VStack, IconButton, useColorMode } from '@chakra-ui/react';
+import { FaSun, FaMoon } from 'react-icons/fa';
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [todos, setTodos] = useState(
+    () => JSON.parse(localStorage.getItem('todos')) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  function deleteTodo(id) {
+    const newTodos = todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    setTodos(newTodos);
+  }
+
+  function addTodo(todo) {
+    setTodos([...todos, todo]);
+  }
+
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <VStack p={4}>
+      <IconButton
+        icon={colorMode === 'light' ? <FaSun /> : <FaMoon />}
+        isRound='true'
+        size='lg'
+        alignSelf='flex-end'
+        onClick={toggleColorMode}
+      />
+      <Heading
+        mb='8'
+        fontWeight='extrabold'
+        size='2xl'
+        bgGradient='linear(to-r, pink.500, pink.300, blue.500)'
+        bgClip='text'
+      >
+        Todo Application
+      </Heading>
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
+      <AddTodo addTodo={addTodo} />
+    </VStack>
+  );
+}
+
+export default App;
